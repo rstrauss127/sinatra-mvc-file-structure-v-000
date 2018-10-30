0f34943912d01b25af8dc5a6fc70ff8019bdb43a
Rebecca Strauss <rstrauss127@gmail.com>
2018-10-30 16:03:47 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,5 +10,12 @@ class ApplicationController < Sinatra::Base
   end
 
   class Dog
+    attr_accessor :name, :breed, :age
+
+    def initialize(name, breed, age)
+      @name = name
+      @breed = breed
+      @age = age
+    end
   end
 end
